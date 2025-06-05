4b5e40776c94c88830579a147104392428ce2c09
Joaquín Rodríguez <joaq.rod0@gmail.com>
2025-06-05 17:28:42 -0300

    prueba para exceso de capacidad

--- tests/pytest/test_preferencias.py
+++ tests/pytest/test_preferencias.py
@@ -67,6 +67,35 @@ def test_algunas_aulas_en_el_edificio_preferido():
     
     assert solver.value(clases_fuera_del_edificio_preferido) == 4
 
+def test_algunas_clases_exceden_capacidad():
+    aulas = make_aulas(
+        dict(capacidad=30),
+        dict(capacidad=40),
+        dict(capacidad=25)
+    )
+
+    clases, modelo = make_clases(
+        len(aulas),
+        dict(cantidad_de_alumnos=31),
+        dict(cantidad_de_alumnos=50),
+        dict(cantidad_de_alumnos=100),
+    )
+
+    cantidad_excedida = preferencias.obtener_cantidad_de_alumnos_fuera_del_aula(modelo, clases, aulas)
+
+    # Forzar asignaciones arbitrarias
+    modelo.add(clases.loc[0, 'aula_asignada'] == 0)
+    modelo.add(clases.loc[1, 'aula_asignada'] == 1)
+    modelo.add(clases.loc[2, 'aula_asignada'] == 2)
+
+    # Resolver
+    solver = cp_model.CpSolver()
+    status = solver.solve(modelo)
+    if status != cp_model.OPTIMAL:
+        pytest.fail(f'El solver terminó con status {solver.status_name(status)}. Alguien escribió mal la prueba.')
+    
+    assert solver.value(cantidad_excedida) == (31 - 30 + 50 - 40 + 100 - 25)
+
 def test_elije_aula_en_edificio_preferido():
     aulas = make_aulas(
         dict(edificio='no preferido 1'),

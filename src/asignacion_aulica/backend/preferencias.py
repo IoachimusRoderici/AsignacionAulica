804ffae6bf2209cfce8d99be94d4e2716180fcd3
Joaquín Rodríguez <joaq.rod0@gmail.com>
2025-06-05 16:53:48 -0300

    Tener en cuenta que hay clases sin preferencia de edificio

--- src/asignacion_aulica/backend/preferencias.py
+++ src/asignacion_aulica/backend/preferencias.py
@@ -42,21 +42,21 @@ def construir_edificios(aulas: DataFrame) -> dict[str, set[int]]:
 
 def obtener_cantidad_de_clases_fuera_del_edificio_preferido(modelo: cp_model.CpModel, clases: DataFrame, aulas: DataFrame):
     '''
-    Agrega al modelo variables booleanas que representan si una clase está fuera de su edificio preferido.
-    Devuelve la suma de esas variables, que representa la cantidad de clases fuera de su edificio preferido.
+    Devuelve una expresión que representa la cantidad de clases fuera de su edificio preferido.
     '''
     edificios = construir_edificios(aulas)
     cantidad_de_clases_fuera_del_edificio_preferido = 0
 
     for clase in clases.itertuples():
-        aulas_del_edificio_preferido_en_un_formato_re_raro = [(aula,) for aula in edificios[clase.edificio_preferido]]
+        if clase.edificio_preferido:
+            aulas_del_edificio_preferido_en_un_formato_re_raro = [(aula,) for aula in edificios[clase.edificio_preferido]]
 
-        # Definir flag de edificio preferido en el modelo
-        fuera_del_edificio_preferido = modelo.new_bool_var(f"{clase.nombre}_fuera_del_edificio_preferido")
-        modelo.add_allowed_assignments([clase.aula_asignada], aulas_del_edificio_preferido_en_un_formato_re_raro).only_enforce_if(~fuera_del_edificio_preferido)
-        modelo.add_forbidden_assignments([clase.aula_asignada], aulas_del_edificio_preferido_en_un_formato_re_raro).only_enforce_if(fuera_del_edificio_preferido)
+            # Definir flag de edificio preferido en el modelo
+            fuera_del_edificio_preferido = modelo.new_bool_var(f"{clase.nombre}_fuera_del_edificio_preferido")
+            modelo.add_allowed_assignments([clase.aula_asignada], aulas_del_edificio_preferido_en_un_formato_re_raro).only_enforce_if(~fuera_del_edificio_preferido)
+            modelo.add_forbidden_assignments([clase.aula_asignada], aulas_del_edificio_preferido_en_un_formato_re_raro).only_enforce_if(fuera_del_edificio_preferido)
 
-        cantidad_de_clases_fuera_del_edificio_preferido += fuera_del_edificio_preferido
+            cantidad_de_clases_fuera_del_edificio_preferido += fuera_del_edificio_preferido
 
     return cantidad_de_clases_fuera_del_edificio_preferido
 

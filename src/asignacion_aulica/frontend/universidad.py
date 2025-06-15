5ac38ac8b28379a60308b8a4030430d161132f8c
vlzJuan <valencellojuan@gmail.com>
2025-06-15 14:38:01 -0300

    Declaracion de metodos primitivos para el uso de edificios y aulas. A implementar, la mayoria

--- src/asignacion_aulica/frontend/universidad.py
+++ src/asignacion_aulica/frontend/universidad.py
@@ -15,19 +15,35 @@ class Universidad:
         self.edificios = edificios
         self.aulas = aulas
 
-    def mostrar_edificios(self):
-        print(self.edificios)
-
-    def mostrar_aulas(self):
-        print(self.aulas)
-
-    def col_names_edificios(self):
+    # Sector de edificios:
+    def col_names_edificios(self): # Retorna lista de columnnames
         print(self.edificios.columns.tolist())
-    
-    def col_names_aulas(self):
+    def mostrar_edificios(self): #TODO implementar, actualmente es un print
+        print(self.edificios)
+    def agregar_edificio(self, row_edificio): #TODO implementar
+        print("A IMPLEMENTAR")
+    def eliminar_edificio(self, id_edificio): #TODO implementar. Prohibir si aulas lo usan
+        print("A IMPLEMENTAR")
+    def modificar_edificio(self, row_edificio): #TODO implementar
+        print("A IMPLEMENTAR")
+    # Sector de aulas
+    def col_names_aulas(self): # Retorna lista de columnnames
         print(self.aulas.columns.tolist())
-
-
+    def mostrar_aulas(self): #TODO implementar, actualmente es un print
+        print(self.aulas)
+    def agregar_aula(self, row): #TODO implementar. Que no permita aulas huerfanas.
+        print("A IMPLEMENTAR")
+    def eliminar_aula(self, id_aula): #TODO implementar
+        print("A IMPLEMENTAR")
+    def modificar_aula(self, row_aula): #TODO implementar
+        print("A IMPLEMENTAR")
+    """Metodo para recuperar el horario de un aula.
+    Si el valor es null en el aula, devuelve el del edificio.
+    Si el edificio esta cerrado en ese horario, #TODO decidir que hacer"""
+    def horario_aula_entrada(self, id_aula): #TODO
+        print("A IMPLEMENTAR")
+    def horario_aula_salida(self, id_aula): #TODO
+        print("A IMPLEMENTAR")
 
 
 def main():

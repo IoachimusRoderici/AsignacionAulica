aef278b000c443b01002591a41210337f7618110
Joaquín Rodríguez <joaq.rod0@gmail.com>
2025-06-20 17:49:16 -0300

    Actualizar prueba de separar asignaciones manuales

--- tests/pytest/test_asignaciones_manuales.py
+++ tests/pytest/test_asignaciones_manuales.py
@@ -6,21 +6,22 @@ from asignacion_aulica.backend.lógica_de_asignación import separar_asignacione
 
 def test_separar_asignaciones_manuales():
     clases = make_clases(
-        dict(aula_asignada=1),
+        dict(aula_asignada=1, día=Día.LUNES, horario_inicio=10, horario_fin=15),
         dict(),
-        dict(aula_asignada=33),
+        dict(aula_asignada=33, día=Día.JUEVES, horario_inicio=255, horario_fin=322),
         dict(),
         dict()
     )
     
-    clases_sin_asignar, indices_sin_asignar, asignaciones_manuales = separar_asignaciones_manuales(clases)
+    clases_sin_asignar, indices_sin_asignar, aulas_ocupadas = separar_asignaciones_manuales(clases)
 
     assert len(clases_sin_asignar) == 3
-    assert list(clases_sin_asignar.index) == [0, 1, 2] # Verificar que los índices se re-generaron
     assert all(clases_sin_asignar['aula_asignada'].isnull())
+    assert list(clases_sin_asignar.index) == [0, 1, 2] # Verificar que los índices se re-generaron
 
     assert indices_sin_asignar == [1, 3, 4]
-    assert asignaciones_manuales == {0: 1, 2: 33}
+
+    assert aulas_ocupadas == {(33, Día.JUEVES, 255, 322), (1, Día.LUNES, 10, 15)}
 
 def test_dos_clases_al_mismo_tiempo_con_una_asignación_manual():
     aulas = make_aulas(

32720799cef3e9b845f903e1c1bdf8960c40b1da
CristianMogensen <cristian.g.mog@gmail.com>
2025-06-20 20:20:01 -0300

    Corregido archivos.py para empezar a implementar las funciones de archivo (están en los TODO) Funciones a implementar: - nuevo_archivo - abrir_archivo - guardar_archivo - importar_archivo - exportar_archivo

--- src/asignacion_aulica/frontend/archivos.py
+++ src/asignacion_aulica/frontend/archivos.py
@@ -8,42 +8,86 @@ También importación y exportación.
 
 import flet as ft
 
-#from universidad import Universidad
+#from universidad import Universidad TODO
 
 
 def nuevo_archivo(page: ft.Page, file_picker: ft.FilePicker):
+    """
+    Funcion ejecutada al hacer click en el botón de Nuevo.
+    Abre una ventana del sistema operativo para elegir la ruta y nombre de
+    archivo para crear un nuevo proyecto.
+    Exporta en: .unrn # TODO
+
+    Parameters
+    ----------
+    page : ft.Page
+        Página principal de flet.
+    file_picker : ft.FilePicker
+        Objeto de flet que se encarga de abrir la ventana y obtener la ruta del
+        archivo.
+
+    Returns
+    -------
+    None.
+
+    """
     # Se instancia la "ventana de archivo".
     # file_picker = ft.FilePicker()
     
     # Función que se llama cuando se selecciona una ruta.
-    def archivo_guardado(e):
+    def resultado_nuevo(e):
         if file_picker.result != None and file_picker.result.path != None:
             ruta = file_picker.result.path
             
+            # TODO
+            # IMPLEMENTAR CREACION DE ARCHIVO NUEVO
             # Se crea un archivo vacío.
             with open(ruta, "w") as f:
                 f.write("")
     
-    file_picker.on_result = archivo_guardado
+    # Función que se llama cuando se clickeó en el botón "Guardar".
+    file_picker.on_result = resultado_nuevo
     
     # Se ejecuta la función para abrir la ventana de archivo.
     file_picker.save_file(
-        dialog_title="Guardar como...",
+        dialog_title="Nuevo archivo",
         file_name="Sin título.unrn",
         allowed_extensions=["unrn"],
     )
 
 def abrir_archivo(page: ft.Page, file_picker: ft.FilePicker):
+    """
+    Funcion ejecutada al hacer click en el botón de Abrir.
+    Abre una ventana del sistema operativo para elegir la ruta y nombre de
+    archivo para abrir un proyecto.
+    Importa en: .unrn # TODO
+
+    Parameters
+    ----------
+    page : ft.Page
+        Página principal de flet.
+    file_picker : ft.FilePicker
+        Objeto de flet que se encarga de abrir la ventana y obtener la ruta del
+        archivo.
+
+    Returns
+    -------
+    None.
+
+    """
     # Función que se llama cuando se selecciona una ruta.
-    def archivo_guardado(e):
+    def resultado_apertura(e):
         if file_picker.result != None and file_picker.result.path != None:
             ruta = file_picker.result.path
             
+            # TODO
+            # IMPLEMENTAR APERTURA DE ARCHIVO
             # Se crea un archivo vacío.
             # with open(ruta, "w") as f:
             #     f.write("")
     
-    file_picker.on_result = archivo_guardado
+    # Función que se llama cuando se clickeó en el botón "Guardar".
+    file_picker.on_result = resultado_apertura
     
     # Se ejecuta la función para abrir la ventana de archivo.
     file_picker.save_file(
@@ -53,19 +97,38 @@ def abrir_archivo(page: ft.Page, file_picker: ft.FilePicker):
     )
 
 def guardar_archivo(page: ft.Page, file_picker: ft.FilePicker):
-    # Se instancia la "ventana de archivo".
-    # file_picker = ft.FilePicker()
-    
+    """
+    Funcion ejecutada al hacer click en el botón de Guardar.
+    Abre una ventana del sistema operativo para elegir la ruta y nombre de
+    archivo para guardar el proyecto.
+    Exporta en: .unrn # TODO
+
+    Parameters
+    ----------
+    page : ft.Page
+        Página principal de flet.
+    file_picker : ft.FilePicker
+        Objeto de flet que se encarga de abrir la ventana y obtener la ruta del
+        archivo.
+
+    Returns
+    -------
+    None.
+
+    """
     # Función que se llama cuando se selecciona una ruta.
-    def archivo_guardado(e):
+    def resultado_guardado(e):
         if file_picker.result != None and file_picker.result.path != None:
             ruta = file_picker.result.path
             
+            # TODO
+            # IMPLEMENTAR GUARDADO DE ARCHIVO
             # Se crea un archivo vacío.
             with open(ruta, "w") as f:
                 f.write("")
     
-    file_picker.on_result = archivo_guardado
+    # Función que se llama cuando se clickeó en el botón "Guardar".
+    file_picker.on_result = resultado_guardado
     
     # Se ejecuta la función para abrir la ventana de archivo.
     file_picker.save_file(
@@ -75,16 +138,38 @@ def guardar_archivo(page: ft.Page, file_picker: ft.FilePicker):
     )
 
 def importar_archivo(page: ft.Page, file_picker: ft.FilePicker):
+    """
+    Funcion ejecutada al hacer click en el botón de Importar.
+    Abre una ventana del sistema operativo para elegir la ruta y nombre de
+    archivo para exportar el proyecto.
+    Importa en: TODO
+
+    Parameters
+    ----------
+    page : ft.Page
+        Página principal de flet.
+    file_picker : ft.FilePicker
+        Objeto de flet que se encarga de abrir la ventana y obtener la ruta del
+        archivo.
+
+    Returns
+    -------
+    None.
+
+    """
     # Función que se llama cuando se selecciona una ruta.
-    def archivo_guardado(e):
+    def resultado_importacion(e):
         if file_picker.result != None and file_picker.result.path != None:
             ruta = file_picker.result.path
             
+            # TODO
+            # IMPLEMENTAR IMPORTACION DE ARCHIVO
             # Se crea un archivo vacío.
             # with open(ruta, "w") as f:
             #     f.write("")
     
-    file_picker.on_result = archivo_guardado
+    # Función que se llama cuando se clickeó en el botón "Guardar".
+    file_picker.on_result = resultado_importacion
     
     # Se ejecuta la función para abrir la ventana de archivo.
     file_picker.save_file(
@@ -94,16 +179,39 @@ def importar_archivo(page: ft.Page, file_picker: ft.FilePicker):
     )
 
 def exportar_archivo(page: ft.Page, file_picker: ft.FilePicker):
+    """
+    Funcion ejecutada al hacer click en el botón de Exportar.
+    Abre una ventana del sistema operativo para elegir la ruta y nombre de
+    archivo para exportar el proyecto.
+    Exporta en: .xlsx, .csv TODO
+
+    Parameters
+    ----------
+    page : ft.Page
+        Página principal de flet.
+    file_picker : ft.FilePicker
+        Objeto de flet que se encarga de abrir la ventana y obtener la ruta del
+        archivo.
+
+    Returns
+    -------
+    None.
+
+    """
+    # TODO
     # Función que se llama cuando se selecciona una ruta.
-    def archivo_guardado(e):
+    def resultado_exportacion(e):
         if file_picker.result != None and file_picker.result.path != None:
             ruta = file_picker.result.path
             
+            # TODO
+            # IMPLEMENTAR EXPORTACION DE ARCHIVO
             # Se crea un archivo vacío.
             # with open(ruta, "w") as f:
             #     f.write("")
     
-    file_picker.on_result = archivo_guardado
+    # Función que se llama cuando se clickeó en el botón "Guardar".
+    file_picker.on_result = resultado_exportacion
     
     # Se ejecuta la función para abrir la ventana de archivo.
     file_picker.save_file(

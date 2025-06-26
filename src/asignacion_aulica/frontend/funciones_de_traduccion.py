94de4f44a630c7e2981ffe58674b68ca2e5d994f
vlzJuan <valencellojuan@gmail.com>
2025-06-26 02:48:33 -0300

    Correccion de errores en funciones_de_traduccion.py

--- src/asignacion_aulica/frontend/funciones_de_traduccion.py
+++ src/asignacion_aulica/frontend/funciones_de_traduccion.py
@@ -1,6 +1,7 @@
 from pandas import DataFrame
 import re
 from asignacion_aulica.backend.dia import Día
+import datetime
 
 def parsear_equipamiento(literal):
     componentes = (x.strip() for x in literal.split(','))
@@ -14,12 +15,16 @@ def horario_a_minutos(cadena):
         horario_cierre = int(matches[2])*60 + int(matches[3])
         return horario_inicio, horario_cierre
 
+
 def horario_simple_a_minutos(cadena):
-    
-    matches = re.findall(r'\d+', cadena) 
-    if len(matches)==2:
-        horario_inicio = int(matches[0])*60 + int(matches[1])
-        return horario_inicio
+    if isinstance(cadena, str):
+        # Extrae horas y minutos de un string como "09:00"
+        matches = re.findall(r'\d+', cadena)
+        if len(matches) >= 2:
+            return int(matches[0]) * 60 + int(matches[1])
+    elif isinstance(cadena, datetime.time):
+        return cadena.hour * 60 + cadena.minute
+
 
 
 def construir_diccionario_de_horarios(aula):
@@ -31,7 +36,7 @@ def construir_diccionario_de_horarios(aula):
     if aula.Martes != "CERRADO":
         horarios[Día.MARTES] = horario_a_minutos(aula.Martes)
     if aula.Miércoles != "CERRADO":
-        horarios[Día.MIÉRCOLES] = horario_a_minutos(aula.Miercoles)
+        horarios[Día.MIÉRCOLES] = horario_a_minutos(aula.Miércoles)
     if aula.Jueves != "CERRADO":
         horarios[Día.JUEVES] = horario_a_minutos(aula.Jueves)
     if aula.Viernes != "CERRADO":
@@ -48,7 +53,7 @@ def traducir_aulas(aulas_frontend:DataFrame):
     aulas_backend['nombre'] = aulas_frontend['Aula']
     aulas_backend['edificio'] = aulas_frontend['Edificio']
     aulas_backend['capacidad'] = aulas_frontend['Capacidad']
-    aulas_backend['equipamiento'] = list(map(parsear_equipamiento, aulas_frontend['equipamiento']))
+    aulas_backend['equipamiento'] = list(map(parsear_equipamiento, aulas_frontend['Equipamiento'].astype(str)))
     aulas_backend['horarios'] = list(map(construir_diccionario_de_horarios, aulas_frontend.itertuples()))
     return aulas_backend
 
@@ -66,11 +71,14 @@ def traducir_clases(clases_frontend:DataFrame):
     
     clases_backend = DataFrame()        
     clases_backend['nombre'] = clases_frontend['Nombre']
-    clases_backend['día'] = mapeador_de_dias[clases_frontend['Día']]
-    clases_backend['horario_inicio']        =   horario_simple_a_minutos(clases_frontend['Horario de inicio'])
-    clases_backend['horario_fin']           =   horario_simple_a_minutos(clases_frontend['Horario de fin'])
+    clases_backend['día'] = clases_frontend['Día'].map(mapeador_de_dias)
+    clases_backend['horario_inicio'] = clases_frontend['Horario de inicio'].apply(horario_simple_a_minutos)
+    clases_backend['horario_fin'] = clases_frontend['Horario de fin'].apply(horario_simple_a_minutos)
     clases_backend['cantidad_de_alumnos']   =   clases_frontend['Cantidad de alumnos']
-    clases_backend['equipamiento_necesario']=   list(map(parsear_equipamiento, clases_frontend['Equipamiento necesario']))
-    clases_backend['edificio_preferido']    =   clases_frontend['Edificio preferencial']
-    clases_backend['aula_asignada']         =   clases_frontend['Aula asignada']
 
+    clases_backend['equipamiento_necesario'] = clases_frontend['Equipamiento necesario'].astype(str).map(parsear_equipamiento)
+    #clases_backend['equipamiento_necesario']=   list(map(parsear_equipamiento, clases_frontend['Equipamiento necesario']))
+    clases_backend['edificio_preferido']    =   clases_frontend['Edificio preferencial']
+    clases_backend['aula_asignada']         =   None
+
+    return clases_backend

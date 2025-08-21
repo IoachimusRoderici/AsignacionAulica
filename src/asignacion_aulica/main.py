0c8033ab4e1dd38b3790352db31fea2bf68da7f3
Joaquín Rodríguez <joaq.rod0@gmail.com>
2025-08-21 20:00:46 -0300

    reubicar módulo de GUI y archivos qml

--- src/asignacion_aulica/main.py
+++ src/asignacion_aulica/main.py
@@ -1,31 +1,5 @@
-import sys, os
-from PySide6.QtGui import QGuiApplication, QFontDatabase
-from PySide6.QtQml import QQmlApplicationEngine
+import sys
+from asignacion_aulica.GUI.main import main
 
-from asignacion_aulica import assets
-
-directorio = os.path.dirname(os.path.abspath(__file__))
-
-def configurar_fuente_por_defecto():
-    default_font_file = assets.get_path('fonts', 'Karla-Regular.ttf')
-    QFontDatabase.addApplicationFont(default_font_file)
-    font = QFontDatabase.font('Karla', 'regular', 12)
-    QGuiApplication.setFont(font)
-
-
-def main() -> int:
-    app = QGuiApplication(sys.argv)
-
-    configurar_fuente_por_defecto()
-
-
-    engine = QQmlApplicationEngine()
-    engine.addImportPath(directorio)
-    engine.loadFromModule('UI', "Main")
-    if engine.rootObjects():
-        exit_code = app.exec()
-    else:
-        exit_code = -1
-
-    del engine
-    return exit_code
+if __name__ == '__main__':
+    sys.exit(main())

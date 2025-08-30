a7a76be568428b1e9b1681a6dd3decb0f39cac66
vlzJuan <valencellojuan@gmail.com>
2025-08-30 13:07:15 -0300

    Agregado de ejemplo para insertar data y crear views

---
+++ src/asignacion_aulica/frontend/Edificios_Data_Insert_Example.sql
@@ -0,0 +1,19 @@
+INSERT INTO "Edificios Data" (
+    "Edificio",
+    "Lunes Inicio", "Lunes Fin",
+    "Martes Inicio", "Martes Fin",
+    "Miércoles Inicio", "Miércoles Fin",
+    "Jueves Inicio", "Jueves Fin",
+    "Viernes Inicio", "Viernes Fin",
+    "Sábado Inicio", "Sábado Fin",
+    "Domingo Inicio", "Domingo Fin"
+) VALUES (
+    'Anasagasti 3',
+    '08:00', '21:00',
+    '08:00', '21:00',
+    '08:00', '121:00',
+    '08:00', '21:00',
+    '08:00', '20:00',
+    '08:00', '13:00',
+    NULL, NULL
+);
